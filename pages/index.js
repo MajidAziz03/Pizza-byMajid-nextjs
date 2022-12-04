f8ab686bd8e902a638ac7ee9549c6d96b8fa1e60
Majid Aziz <azizmajid34@gmail.com>
2022-12-04 13:10:29 +0500

    Creating Pizza List Component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Featured from '../Components/Featured'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -10,8 +11,8 @@ export default function Home() {
       <title>Pizza-Majid</title>
       <meta name="description" content="Best Pizza in the Town" />
       <link rel="icon" href="/favicon.ico" />
-    </Head>
-homepage
+      </Head>
+      <Featured />
 </>
   )
 }

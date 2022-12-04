0ee7b6c161e1a3cbf3e5402a4b35a921a955d362
Majid Aziz <azizmajid34@gmail.com>
2022-12-04 11:53:47 +0500

    Navbar first item Completed

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <div className={styles.container}>
             <div className={styles.item}>
-                <div className={styles.telephone}>
+                <div className={styles.callButton}>
                     <Image src='/img/telephone.png' width={'32'} height={'32'} /> 
                 </div>
                 <div className={styles.texts}>
@@ -14,8 +14,8 @@ const Navbar = () => {
                     <div className={styles.text}>021-111-111</div>
                 </div>
             </div>
-            <div className={styles.item}></div>
-            <div className={styles.item}></div>
+            <div className={styles.item}>cneter</div>
+            <div className={styles.item}>right</div>
         </div>
     )
 }

a1d5e85c44c57de86e395f19291256fd6fdbfb20
Majid Aziz <azizmajid34@gmail.com>
2022-12-04 12:02:18 +0500

    Completed Navbar Second Item

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -14,7 +14,17 @@ const Navbar = () => {
                     <div className={styles.text}>021-111-111</div>
                 </div>
             </div>
-            <div className={styles.item}>cneter</div>
+            <div className={styles.item}>
+                <ul className={styles.list}>
+                    <li className={styles.listItem}>Homepage</li>
+                    <li className={styles.listItem}>Products</li>
+                    <li className={styles.listItem}>Menu</li>
+                    <Image src='/img/logo.png' width='160' height='69'/> 
+                    <li className={styles.listItem}>Events</li>
+                    <li className={styles.listItem}>Blog</li>
+                    <li className={styles.listItem}>Contact</li>
+                </ul>
+            </div>
             <div className={styles.item}>right</div>
         </div>
     )

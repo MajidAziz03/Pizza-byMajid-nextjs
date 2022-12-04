bb16cfe1ac75ceac97d62c4af8bd7ba4a0260d7c
Majid Aziz <azizmajid34@gmail.com>
2022-12-04 13:38:56 +0500

    Some Errors in Pizza will fix soon

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Featured from '../Components/Featured'
-import styles from '../styles/Home.module.css'
+import PizzaList from '../Components/PizzaList'
 
 export default function Home() {
   return (
@@ -13,6 +13,7 @@ export default function Home() {
       <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      <PizzaList />
 </>
   )
 }
